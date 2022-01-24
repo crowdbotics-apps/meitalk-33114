c59331bb14a520f3b24b9d3a8e35eda8a9051844
Crowdbotics <team@crowdbotics.com>
2022-01-24 03:39:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "meitalk_33114",
+  url: "https://meitalk_33114.botics.co",
+  api: "https://meitalk_33114.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
